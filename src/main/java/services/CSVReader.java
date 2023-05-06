7e80fd6709326c978d7c303caf85cfdd5a7b8721
Vakaris Paulavicius <vakaris.paulavicius@kcl.ac.uk>
2023-05-06 23:24:58 +0100

    Refactor CSVReader

--- src/main/java/services/CSVReader.java
+++ src/main/java/services/CSVReader.java
@@ -1,6 +1,5 @@
 package services;
 
-import com.sun.source.tree.Tree;
 import entities.Customer;
 import entities.Investor;
 import entities.Mortgage;
@@ -9,7 +8,6 @@ import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import uk.landbay.Main;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -21,15 +19,9 @@ public class CSVReader {
     private static final Logger logger = LoggerFactory.getLogger(CSVReader.class);
 
     public static List<Investor> readInvestors(String filename) throws IOException {
-        logger.info(String.format("Reading the investors from file %s", filename));
+        CSVParser parser = readFile(filename);
         // Store references to investors temporarily
         Map<String, Investor> investorsMap = new HashMap<>();
-        InputStream inputStream = CSVReader.class.getResourceAsStream(filename);
-        if (inputStream == null) {
-            throw new IOException(String.format("File %s does not exist", filename));
-        }
-        CSVParser parser = new CSVParser(new InputStreamReader(inputStream), CSVFormat.DEFAULT.builder().setHeader().setSkipHeaderRecord(true).build());
-
         /* Read all investors and create an object for each. Store references to the objects with
            their name as the key to allow for adding the products to their lists later */
         for (CSVRecord entry : parser) {
@@ -50,15 +42,9 @@ public class CSVReader {
     }
 
     public static List<Customer> readCustomers(String filename) throws IOException {
-        logger.info(String.format("Reading the customers from file %s", filename));
+        CSVParser parser = readFile(filename);
         // Store references to customers temporarily
         Map<String, Customer> customersMap = new HashMap<>();
-        InputStream inputStream = CSVReader.class.getResourceAsStream(filename);
-        if (inputStream == null) {
-            throw new IOException(String.format("File %s does not exist", filename));
-        }
-        CSVParser parser = new CSVParser(new InputStreamReader(inputStream), CSVFormat.DEFAULT.builder().setHeader().setSkipHeaderRecord(true).build());
-
         /* Read all customers and create an object for each. Store references to the objects with
            their name as the key to allow for adding the mortgages later */
         for (CSVRecord entry : parser) {
@@ -79,19 +65,22 @@ public class CSVReader {
     }
 
     public static Map<String, Double> readProducts(String filename) throws IOException {
-        logger.info(String.format("Reading the products from file %s", filename));
+        CSVParser parser = readFile(filename);
         Map<String, Double> products = new TreeMap<>();
-        InputStream inputStream = Main.class.getResourceAsStream(filename);
-        if (inputStream == null) {
-            throw new IOException(String.format("File %s does not exist", filename));
-        }
-        CSVParser parser = new CSVParser(new InputStreamReader(inputStream), CSVFormat.DEFAULT.builder().setHeader().setSkipHeaderRecord(true).build());
-
         for (CSVRecord entry : parser) {
             String name = entry.get(0);
             double value = Double.parseDouble(entry.get(1).replaceAll("%", ""));
             products.put(name, value);
         }
         return products;
+    }
+
+    private static CSVParser readFile(String filename) throws IOException{
+        logger.info(String.format("Reading from file %s", filename));
+        InputStream inputStream = CSVReader.class.getResourceAsStream(filename);
+        if (inputStream == null) {
+            throw new IOException(String.format("File %s does not exist", filename));
+        }
+        return new CSVParser(new InputStreamReader(inputStream), CSVFormat.DEFAULT.builder().setHeader().setSkipHeaderRecord(true).build());
     }
 }
