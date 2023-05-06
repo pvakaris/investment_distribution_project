e3868822b43781a3c9b465d6c7915bb92b48ccd1
Vakaris Paulavicius <vakaris.paulavicius@kcl.ac.uk>
2023-05-06 20:14:47 +0100

    Read the investors CSV file

--- src/main/java/uk/landbay/Main.java
+++ src/main/java/uk/landbay/Main.java
@@ -1,7 +1,12 @@
 package uk.landbay;
 
+import entities.Investor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import services.CSVReader;
+
+import java.io.IOException;
+import java.util.List;
 
 public class Main {
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
@@ -16,5 +21,18 @@ public class Main {
     }
 
     private static void runAlgorithm() {
+        try {
+            List<Investor> investors = CSVReader.readInvestors(Main.class.getResourceAsStream("/funded_products_by_funder.csv"));
+            for(Investor inv : investors) {
+                System.out.println(inv.getName());
+            }
+        }
+        catch (IOException exception) {
+            logger.error("An exception occurred when reading the investors file", exception);
+        }
+        catch (Exception exception) {
+            logger.error("An unknown error occurred", exception);
+        }
+
     }
 }
