56fbfda58bffdf0b7521e5a5e83357ef36f7273c
Vakaris Paulavicius <vakaris.paulavicius@kcl.ac.uk>
2023-05-06 20:26:30 +0100

    Add Partner superclass

---
+++ src/main/java/entities/Partner.java
@@ -0,0 +1,14 @@
+package entities;
+
+public class Partner {
+
+    private final String name;
+
+    public Partner(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
