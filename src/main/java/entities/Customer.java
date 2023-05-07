33b2cb095515c421ddb8bf0281f36739f7b106d8
Vakaris Paulavicius <vakaris.paulavicius@kcl.ac.uk>
2023-05-07 20:57:14 +0100

    Add documentation for Customer

--- src/main/java/entities/Customer.java
+++ src/main/java/entities/Customer.java
@@ -3,20 +3,38 @@ package entities;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * A class representing a customer
+ *
+ * @author Vakaris Paulavicius
+ * @version 1.0
+ */
 public class Customer extends Partner {
+    // A list that holds all the orders a Customer has
+    private final List<Order> orders;
 
-    private final List<Mortgage> mortgages;
-
+    /**
+     * Creates a new customer with the given name.
+     * @param name the name of the customer
+     */
     public Customer(String name) {
         super(name);
-        mortgages = new ArrayList<>();
+        orders = new ArrayList<>();
     }
 
-    public List<Mortgage> getMortgages() {
-        return mortgages;
+    /**
+     * Returns the list of orders the Customer has
+     * @return the list of orders
+     */
+    public List<Order> getOrders() {
+        return orders;
     }
 
-    public void addMortgage(Mortgage newMortgage) {
-        mortgages.add(newMortgage);
+    /**
+     * Adds a new order to the list of orders
+     * @param newOrder the new order to add
+     */
+    public void addOrder(Order newOrder) {
+        orders.add(newOrder);
     }
 }
