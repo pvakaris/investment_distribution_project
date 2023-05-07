92e47028d2b9cae8516bedbb784eafccb9c108ba
Vakaris Paulavicius <vakaris.paulavicius@kcl.ac.uk>
2023-05-07 20:56:06 +0100

    Add documentation for Partner

--- src/main/java/entities/Partner.java
+++ src/main/java/entities/Partner.java
@@ -1,14 +1,28 @@
 package entities;
 
+/**
+ * A partner (both customers and investors) THE SUPERCLASS
+ *
+ * @author Vakaris Paulavicius
+ * @version 1.0
+ */
 public class Partner {
-
     private final String name;
 
+    /**
+     * Constructs a partner with the given name.
+     * @param name the name of the partner
+     */
     public Partner(String name) {
         this.name = name;
     }
 
+    /**
+     * Returns the name of the partner.
+     * @return the name of the partner
+     */
     public String getName() {
         return name;
     }
 }
+
