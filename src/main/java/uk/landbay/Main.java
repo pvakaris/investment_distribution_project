e8cf4547bca5372b5e957ef995c3b3219894366d
Vakaris Paulavicius <vakaris.paulavicius@kcl.ac.uk>
2023-05-07 20:55:34 +0100

    Add documentation in main

--- src/main/java/uk/landbay/Main.java
+++ src/main/java/uk/landbay/Main.java
@@ -2,19 +2,32 @@ package uk.landbay;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import services.Calculator;
+import services.Executor;
 
 import java.io.IOException;
 import java.util.Properties;
 
+/**
+ * The starting point of the application
+ *
+ * @author Vakaris Paulavicius
+ * @version 1.0
+ */
 public class Main {
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+    /**
+     * Method that starts the application
+     * @param args the default
+     */
     public static void main(String[] args) {
         logger.info("The program has started");
         try {
+            // Retrieve the properties
             Properties properties = Main.getProperties();
-            Calculator calculator = new Calculator(properties);
-            calculator.start();
+            // Initialise the executor and execute the task
+            Executor executor = new Executor(properties);
+            executor.run();
         }
         catch (Exception exception) {
             logger.error("An exception occurred", exception);
@@ -24,10 +37,14 @@ public class Main {
         }
     }
 
+    /**
+     * Extract properties from the resources and returns them
+     * @return a Properties object containing important resources
+     */
     private static Properties getProperties () {
         Properties props = new Properties();
         try {
-            props.load(Calculator.class.getClassLoader().getResourceAsStream("config.properties"));
+            props.load(Executor.class.getClassLoader().getResourceAsStream("config.properties"));
         }
         catch (IOException exception) {
             logger.error("Could not load the config.properties file");
