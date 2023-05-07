06f5c32958109dffa2fc905d129724d63a2a3b87
Vakaris Paulavicius <vakaris.paulavicius@kcl.ac.uk>
2023-05-07 20:54:44 +0100

    Add documentation for CSVReader

--- src/main/java/services/CSVReader.java
+++ src/main/java/services/CSVReader.java
@@ -2,7 +2,7 @@ package services;
 
 import entities.Customer;
 import entities.Investor;
-import entities.Mortgage;
+import entities.Order;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
@@ -14,10 +14,23 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.*;
 
+/**
+ * A class that handles the CSV input files
+ *
+ * @author Vakaris Paulavicius
+ * @version 1.0
+ */
 public class CSVReader {
 
     private static final Logger logger = LoggerFactory.getLogger(CSVReader.class);
 
+    /**
+     * Reads investor data from a CSV file and returns a list of Investor objects.
+     *
+     * @param filename the name of the CSV file containing the investor data
+     * @return a list of Investor objects containing the data from the CSV file
+     * @throws IOException if there is an error reading the file
+     */
     public static List<Investor> readInvestors(String filename) throws IOException {
         CSVParser parser = readFile(filename);
         // Store references to investors temporarily
@@ -34,13 +47,20 @@ public class CSVReader {
                 investorsMap.put(name, investor);
             }
 
-            investor.addProduct(product);
+            investor.addWish(product);
         }
 
         // Finally return the complete list of investors
         return new ArrayList<>(investorsMap.values());
     }
 
+    /**
+     * Reads customer data from a CSV file and returns a list of Customer objects.
+     *
+     * @param filename the name of the CSV file to read from
+     * @return a list of Customer objects representing the data in the file
+     * @throws IOException if the file cannot be read
+     */
     public static List<Customer> readCustomers(String filename) throws IOException {
         CSVParser parser = readFile(filename);
         // Store references to customers temporarily
@@ -49,7 +69,6 @@ public class CSVReader {
            their name as the key to allow for adding the mortgages later */
         for (CSVRecord entry : parser) {
             String name = entry.get(0);
-            Mortgage mortgage = new Mortgage(entry.get(1), Double.parseDouble(entry.get(2)));
 
             Customer customer = customersMap.get(name);
             if (customer == null) {
@@ -57,13 +76,19 @@ public class CSVReader {
                 customersMap.put(name, customer);
             }
 
-            customer.addMortgage(mortgage);
+            customer.addOrder(new Order(entry.get(1), Double.parseDouble(entry.get(2)), customer));
         }
 
         // Finally return the complete list of investors
         return new ArrayList<>(customersMap.values());
     }
 
+    /**
+     * Read products from a file and return a map containing (productID, interest) pairs
+     * @param filename The name of the file
+     * @return A map of (productID, interest) pairs
+     * @throws IOException if the file cannot be read
+     */
     public static Map<String, Double> readProducts(String filename) throws IOException {
         CSVParser parser = readFile(filename);
         Map<String, Double> products = new TreeMap<>();
@@ -75,6 +100,13 @@ public class CSVReader {
         return products;
     }
 
+    /**
+     * Reads a CSV file and returns a CSVParser object for parsing the data.
+     *
+     * @param filename the name of the CSV file to be read
+     * @return a CSVParser object containing the parsed data from the file
+     * @throws IOException if the file does not exist or cannot be read
+     */
     private static CSVParser readFile(String filename) throws IOException{
         logger.info(String.format("Reading from file %s", filename));
         InputStream inputStream = CSVReader.class.getResourceAsStream(filename);
