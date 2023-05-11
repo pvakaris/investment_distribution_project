15d44ef78ee01acc03b96800edf243da165ffe4d
Vakaris Paulavicius <vakaris.paulavicius@kcl.ac.uk>
2023-05-12 00:25:45 +0100

    Remover redundant comments

--- src/main/java/algorithms/GreedyAlgorithm.java
+++ src/main/java/algorithms/GreedyAlgorithm.java
@@ -17,65 +17,48 @@ import java.util.*;
  * next investment.
  *
  * The time complexity though is quite large: O(n log n) on average and O(n^2 log n) in the worst case.
- *
- * @author Vakaris Paulavicius
- * @version 1.0
  */
-public class GreedyAlgorithm implements Algorithm {
+public class GreedyAlgorithm implements AbstractAlgorithm {
 
     private static final Logger logger = LoggerFactory.getLogger(GreedyAlgorithm.class);
 
     /**
      * The method that does the greedy search for a solution
-     * @param investorsList a list of investors looking to invest
+     * @param investors a queue of investors looking to invest
      * @param investments a list of investments available for investors
      * @return AlgorithmResult containing the execution results
      * @throws RuntimeException if something horrible happens
      */
     @Override
-    public AlgorithmResult run(List<Investor> investorsList, List<Investment> investments) throws RuntimeException {
+    public AlgorithmResult run(Queue<Investor> investors, List<Investment> investments) {
         logger.info("The greedy algorithm has started");
-        // Map that will hold the investment assignments
-        Map<Investor, List<Investment>> results = new HashMap<>();
         List<Investment> unusedInvestments = new LinkedList<>();
 
         // Sort investments by the amount of money they require (the most expensive at the beginning)
-        Collections.sort(investments);
-
-        // Create a comparator to compare two investors by the amount of money they will gain from their investments annually
-        Comparator<Investor> investorComparator = Comparator.comparingDouble(investor -> {
-            List<Investment> inv = results.getOrDefault(investor, Collections.emptyList());
-            double sum = 0.0;
-            for (Investment investment : inv) {
-                sum += investment.getAnnualYield();
-            }
-            return sum;
-        });
+        Arrays.sort(investments.toArray(new Investment[0]), Collections.reverseOrder());
 
         // For each investment find an investor
         for (Investment investment : investments) {
             boolean assigned = false;
-            for (Investor investor : investorsList) {
+            Stack<Investor> tempInvestorStack = new Stack<>();
+            while (!investors.isEmpty()) {
+                Investor investor = investors.poll();
+                tempInvestorStack.push(investor);
                 if (investor.getWishlist().contains(investment.getProductID())) {
-                    if (results.containsKey(investor)) {
-                        results.get(investor).add(investment);
-                    } else {
-                        results.put(investor, new LinkedList<>(List.of(investment)));
-                    }
-                    // Remove the wish from the investor's wishlist
+                    // Add investment to the investor's portfolio and remove from the wishlist
                     investor.removeWish(investment.getProductID());
+                    investor.addInvestment(investment);
                     assigned = true;
-                    // After an investment was added, resort the list so that those investors who earn the least, get the privilege in the next iteration
-                    investorsList.sort(investorComparator);
                     break;
                 }
             }
-            if (!assigned) {
-                // If this place was reached, it means that none of the investors wanted the investment. Add to the unused list.
-                unusedInvestments.add(investment);
+            if (!assigned) unusedInvestments.add(investment);
+            while(!tempInvestorStack.isEmpty()) {
+                investors.add(tempInvestorStack.pop());
             }
         }
         logger.info("The greedy algorithm has finished. Returning the result.");
-        return new AlgorithmResult(results, unusedInvestments);
+        return new AlgorithmResult(investors, unusedInvestments);
     }
+
 }
