a27d6636d57918200bfb9801798259906de52a30
Vakaris Paulavicius <vakaris.paulavicius@kcl.ac.uk>
2023-05-12 00:23:21 +0100

    Add ChromosomeType enum

---
+++ src/main/java/algorithms/genetic_algorithm_extra/ChromosomeType.java
@@ -0,0 +1,8 @@
+package algorithms.genetic_algorithm_extra;
+
+/**
+ * The type of the chromosome. Used with chromosome factory to create chromosomes of a specific type
+ */
+public enum ChromosomeType {
+    HARD_BOUNDARY;
+}
