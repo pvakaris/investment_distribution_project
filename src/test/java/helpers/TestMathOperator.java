526cbffd6cd7e8c6502ff3ffa6067717fd9611b1
Vakaris Paulavicius <vakaris.paulavicius@kcl.ac.uk>
2023-05-12 12:18:26 +0100

    Make test methods public

--- src/test/java/helpers/TestMathOperator.java
+++ src/test/java/helpers/TestMathOperator.java
@@ -11,28 +11,28 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TestMathOperator {
 
     @Test
-    void testGetStandardDeviation() {
+    public void testGetStandardDeviation() {
         List<Double> numbers = Arrays.asList(5.0, 9.0, 6.0, 6.0);
         double deviation = MathOperator.getStandardDeviation(numbers);
         assertEquals(1.5, deviation);
     }
 
     @Test
-    void testGetStandardDeviationWithEmptyList() {
+    public void testGetStandardDeviationWithEmptyList() {
         List<Double> numbers = List.of();
         double deviation = MathOperator.getStandardDeviation(numbers);
         assertEquals(0.0, deviation);
     }
 
     @Test
-    void testGetStandardDeviationWithOneElement() {
+    public void testGetStandardDeviationWithOneElement() {
         List<Double> numbers = List.of(5.0);
         double deviation = MathOperator.getStandardDeviation(numbers);
         assertEquals(0.0, deviation);
     }
 
     @Test
-    void testGetStandardDeviationWithNegativeNumbers() {
+    public void testGetStandardDeviationWithNegativeNumbers() {
         List<Double> numbers = Arrays.asList(-9.0, -5.0, -6.0, -8.0, -15.0);
         double deviation = MathOperator.getStandardDeviation(numbers);
         assertEquals(3.5, Double.parseDouble(new DecimalFormat("#.#").format(deviation)));
